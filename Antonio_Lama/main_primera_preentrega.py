fc7599c0c284da44506128fc380ee51de562e261
TonoLama <alama@prueba.cl>
2023-01-30 09:01:01 -0300

    Try/except

--- Antonio_Lama/main_primera_preentrega.py
+++ Antonio_Lama/main_primera_preentrega.py
@@ -1,43 +1,49 @@
 ''' PRE_ENTREGA N°1 -> Crear un programa para registro e inicio de sesión '''
-from Antonio_Lama.Primera_pre_entrega.utils import *
+try:
+    from utils import *
+except Exception:
+    print('Debes instalar el paquete Antonio_Lama-1.0 antes de arrancar')
+else:
+    menu = ''' \n
+    Selecciona del Menú alguna de estas opciones: 
 
-menu = ''' \n
-Selecciona del Menú alguna de estas opciones: 
+    -------- Menú ---------- Letra -----
 
--------- Menú ---------- Letra -----
+    * Iniciar sesión    ->   ( L )
+    * Nuevo registro    ->   ( N )
+    * Mostrar registros ->   ( S )
+    * Terminar          ->   ( Q )
 
-* Iniciar sesión    ->   ( L )
-* Nuevo registro    ->   ( N )
-* Mostrar registros ->   ( S )
-* Terminar          ->   ( Q )
-
-------------------------------------\n'''
-
-
-def run():
-    '''Función principal'''
-    bandera = True
-    print('\n', '=' * 35)
-    print('\tHola, Bienvenido !!!')
-    print('', '=' * 35)
-
-    while bandera:
-        print(menu)
-        action = input('Escribe tu opción   ->   usar Letra: ')
-        if action.upper() == 'N':
-            new_user()
-        elif action.upper() == 'Q':
-            print('\nYa te vas? Nos vemos pronto!\n')
-            bandera = False
-        elif action.upper() == 'S':
-            show_users()
-        elif action.upper() == 'L':
-            if login() == True:
-                desktop()
-        elif action.upper() == 'CLEAR': # no lo muestro en el menú. Si se usa se debe volver a crear el ADMIN
-            base_datos_dict.clear()
-        else:
-            print('\n** ATENCIÓN! No existe esa opción en el menú, vuelve a escoger. **')
+    ------------------------------------\n'''
 
 
-run()
+    def run():
+        '''Función principal'''
+        bandera = True
+        print('\n', '=' * 35)
+        print('\tHola, Bienvenido !!!')
+        print('', '=' * 35)
+
+        while bandera:
+            print(menu)
+            action = input('Escribe tu opción   ->   usar Letra: ')
+            if action.upper() == 'N':
+                new_user()
+            elif action.upper() == 'Q':
+                print('\nYa te vas? Nos vemos pronto!\n')
+                bandera = False
+            elif action.upper() == 'S':
+                show_users()
+            elif action.upper() == 'L':
+                if login() == True:
+                    desktop()
+            elif action.upper() == 'CLEAR': # no lo muestro en el menú. Si se usa se debe volver a crear el ADMIN
+                base_datos_dict.clear()
+            else:
+                print('\n** ATENCIÓN! No existe esa opción en el menú, vuelve a escoger. **')
+
+
+    run()
+
+finally:
+    print('Terminado')
--- Antonio_Lama/main_segunda_preentrega.py
+++ Antonio_Lama/main_segunda_preentrega.py
@@ -1,14 +1,21 @@
-from Antonio_Lama.Segunda_pre_entrega.Class_utils import Client
 
+try:
+    from Antonio_Lama.Segunda_pre_entrega.Class_utils import Client
+except Exception:
+    print('** Debes instalar el paquete Antonio_Lama-1.0 antes de arrancar **')
+else:
 
-cliente1 = Client()
+    cliente1 = Client()
 
-cliente1.new_account()
-print(cliente1)
+    cliente1.new_account()
+    print(cliente1)
 
-card1 = cliente1.show_card()
-print(card1)
+    card1 = cliente1.show_card()
+    print(card1)
 
-pay = cliente1.pay()
-print(pay)
+    pay = cliente1.pay()
+    print(pay)
+
+finally:
+    print('Terminado')
 
